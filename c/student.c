667d55cf1d39530f3e1edbf5d1e5848e5c9c8c1f
satish <rodd.satish@gmail.com>
2017-09-23 18:02:16 +0400

    commit

--- c/student.c
+++ c/student.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-
+#include"compar_string.c"
 struct employee 
 {
 	char 	name[10]; 	// employee name.
@@ -15,25 +15,25 @@ struct employee
 struct employee * 
 create_employee() 
 {
-    struct employee *s1;
-    s1=malloc(sizeof(struct employee));
-    return(s1);
+    struct employee *emp;
+    emp=malloc(sizeof(struct employee));
+    return(emp);
 }
 
 /*
  *employee info.
  */
 void 
-employee_input(struct employee *s1)
+employee_input(struct employee *emp)
 {
     int exit = 0;
     /*
      *assigning input values
      */
     printf("enter employee name: ");
-    scanf("%s",s1->name);
+    scanf("%s",emp->name);
     printf("enter employee id: ");
-    scanf("\n%d",& s1->id);
+    scanf("\n%d",& emp->id);
 
     /*
      * check salary if it is less than 0 or greater
@@ -41,28 +41,41 @@ employee_input(struct employee *s1)
      */
     while (!exit) {
     	printf("enter employee salary: ");
-    	scanf("\n%d",&s1->salary);
+    	scanf("\n%d",&emp->salary);
 	exit = 1;
-	if (s1->salary < 0  | s1->salary >10000){
+	if (emp->salary < 0  | emp->salary >10000){
 	   printf("salary is out of range. enter correct value.\n");
            exit = 0;
 	  }
     }
 }
 void 
-print_output(struct employee *s1)
+print_output(struct employee *emp)
 {
 	/*
 	 * printing the employee details.
 	 */
-    printf("%s, %d, %d",s1->name,s1->id,s1->salary);
+    printf("%s, %d, %d",emp->name,emp->id,emp->salary);
 }
   
 void 
 main()
 {
-    struct employee *s1;
-    s1 = create_employee();
-    employee_input(s1);
-    print_output(s1);
+    int n,i,found;
+    char search[10];
+    struct employee *emp[10];
+    printf("Enter the number of employee:");
+    scanf("%d",&n);
+    for (i=0; i<n; i++){
+        emp[i] = create_employee();
+        employee_input(emp[i]);
+    }
+    printf("Enter the name of employee to search:");
+    scanf("%s",search);
+    for (i=0; i<n; i++) {
+         found = string_compare(search, emp[i]->name);
+         if (found) {
+             print_output(emp[i]);
+         }
+    }
 }
